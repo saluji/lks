e3434c9752d523459338e9a0e597e7cfc54448a8
Erwin <erwin_oudomvylay@hotmail.de>
2024-06-28 18:22:06 +0200

    restart button

--- GameJam_SS24/Assets/Scripts/Logic/LevelManager.cs
+++ GameJam_SS24/Assets/Scripts/Logic/LevelManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    [SerializeField] PauseManager pauseManager;
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -14,5 +15,10 @@ public class LevelManager : MonoBehaviour
     public void TitelScreen()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        pauseManager.Resume();
     }
 }
--- GameJam_SS24/Assets/Scripts/Logic/UIManager.cs
+++ GameJam_SS24/Assets/Scripts/Logic/UIManager.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] GameObject menuPanel;
+    [SerializeField] GameObject hUDPanel;
+
     public void TogglePause(bool isPaused)
     {
         menuPanel.SetActive(isPaused);
+        hUDPanel.SetActive(!isPaused);
     }
 }
