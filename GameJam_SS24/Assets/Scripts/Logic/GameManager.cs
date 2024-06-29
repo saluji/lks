9373f262817bede0c0a74d1dc108502f4affd2af
Erwin <erwin_oudomvylay@hotmail.de>
2024-06-30 01:15:49 +0200

    death state machine #2

--- GameJam_SS24/Assets/Scripts/Logic/GameManager.cs
+++ GameJam_SS24/Assets/Scripts/Logic/GameManager.cs
@@ -1,14 +1,22 @@
+using System.Collections;
 using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] CursorManager cursorManager;
     [SerializeField] UIManager uIManager;
+    float gameOverLength = 4;
 
     public void GameOver()
     {
+        StartCoroutine(GameOverCountdown());
         PauseManager.isPaused = true;
         Time.timeScale = 0;
         uIManager.ToggleGameOver();
         cursorManager.ShowCursor();
+    }
+
+    IEnumerator GameOverCountdown()
+    {
+        yield return new WaitForSeconds(4);
     }
 }
