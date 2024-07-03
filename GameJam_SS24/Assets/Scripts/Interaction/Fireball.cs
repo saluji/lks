bf6de6cd6bcf4ae26f63f567c601a5c0a1f4c135
Erwin <erwin_oudomvylay@hotmail.de>
2024-07-03 16:15:19 +0200

    spawn manager

--- GameJam_SS24/Assets/Scripts/Interaction/Fireball.cs
+++ GameJam_SS24/Assets/Scripts/Interaction/Fireball.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class Fireball : MonoBehaviour
 {
-    [SerializeField] GameObject[] pieces;
-    [SerializeField] float explosionForce = 5f;
-    [SerializeField] float explosionRadius = 5f;
-
     [SerializeField] float speed = 10;
-
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
@@ -24,24 +14,5 @@ public class Fireball : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        //     if (collider.gameObject.CompareTag("Destructable"))
-        //     {
-        //         BreakObject();
-        //     }
-        // }
-        // void BreakObject()
-        // {
-        //     gameObject.SetActive(false);
-
-        //     foreach (GameObject piece in pieces)
-        //     {
-        //         GameObject instantiatedPiece = Instantiate(piece, transform.position, transform.rotation);
-        //         instantiatedPiece.AddComponent<Rigidbody>();
-        //         Rigidbody rb = instantiatedPiece.GetComponent<Rigidbody>();
-        //         if (rb != null)
-        //         {
-        //             rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
-        //         }
-        //     }
     }
 }
