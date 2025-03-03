16d1e136b54869cb216e45e0221a64f295caa5a3
Erwin <erwin_oudomvylay@hotmail.de>
2025-03-03 13:28:05 +0100

    fixed spawn limiter

--- GameJam_SS24/Assets/Scripts/Logic/SpawnManager.cs
+++ GameJam_SS24/Assets/Scripts/Logic/SpawnManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -44,5 +44,35 @@ public class SpawnManager : MonoBehaviour
         yield return new WaitForSeconds(spawnDelay);
         SpawnKnight();
         SpawnNPC();
+    }
+}
+*/
+
+using System.Collections;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] GameObject knight;
+    [SerializeField] Transform[] knightSpawnPoints;
+    [SerializeField] float spawnDelay = 2f;
+    [SerializeField] int maxKnights = 10; // Set a limit
+
+    private int knightCount = 0;
+
+    void Start()
+    {
+        StartCoroutine(SpawnKnight());
+    }
+
+    IEnumerator SpawnKnight()
+    {
+        while (knightCount < maxKnights)
+        {
+            int randomIndex = Random.Range(0, knightSpawnPoints.Length);
+            Instantiate(knight, knightSpawnPoints[randomIndex].position, knightSpawnPoints[randomIndex].rotation);
+            knightCount++; // Track the number of spawned knights
+            yield return new WaitForSeconds(spawnDelay);
+        }
     }
 }
